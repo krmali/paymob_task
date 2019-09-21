efbd3aa26a9b4717d9d32926f8c172c86b1e392b
kareem <kareem@k.com>
2019-09-21 23:38:34 +0200

    small change

--- paymob_messages/views.py
+++ paymob_messages/views.py
@@ -1,10 +1,3 @@
-# from django.shortcuts import render
-
-
-
-# from django.views.generic import View, UpdateView
-# from .forms import SignupForm
-
 from django.views.generic import View, UpdateView
 from rest_framework import viewsets, permissions
 from .models import Message
