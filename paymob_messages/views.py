b38e8d2faddae2ee9edfcb6846725b9c60357155
kareem <kareem@k.com>
2019-09-21 05:57:23 +0200

    add permissions

--- paymob_messages/views.py
+++ paymob_messages/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Message
 from .serializers import MessageSerializer
 
 class MessageView(viewsets.ModelViewSet):
 	queryset = Message.objects.all()
 	serializer_class = MessageSerializer
+	#permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
